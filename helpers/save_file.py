7a9039ec80ea36a76aaefd0dff0ce2b431e9f7d8
Robert Druska <robert.druska@gmail.com>
2025-05-18 13:02:10 +0200

    remove old code

--- helpers/save_file.py
+++
@@ -1,6 +0,0 @@
-import json
-
-def save_file(path, data):
-  with open(path, 'w', encoding='utf8') as f:
-    json.dump(data, f, indent=4, ensure_ascii=False)
-  print('Data saved successfully to {}'.format(path))
